b64b618779a1d21094956814d3502ef7a6938290
Lam Nhan <lamnhan92@gmail.com>
2021-01-29 15:48:20 +0700

    refactor app & nav service

--- projects/useful/src/lib/services/app/app.service.ts
+++ projects/useful/src/lib/services/app/app.service.ts
@@ -1,20 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+
+export interface AppCustomMetas {
+  title?: string;
+  description?: string;
+  image?: string;
+  url?: string;
+  author?: string;
+  twitterCard?: string;
+  twitterCreator?: string;
+  ogType?: string;
+  ogLocale?: string;
+}
+
+export interface AppMetas extends AppCustomMetas {
+  twitterSite?: string;
+  ogSiteName?: string;
+  fbAppId?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
   private data: {[key: string]: unknown} = {};
+  private defaultMetas: AppMetas = {};
 
-  constructor() {}
+  constructor(
+    private title: Title,
+    private meta: Meta,
+  ) {}
 
   init(
-    data: Record<string, unknown> = {}
+    data: Record<string, unknown> = {},
+    defaultMetas: AppMetas = {},
   ) {
     this.data = { ...this.data, ...data };
-    // built-in meta
-    this.setViewPort();
-    this.setHost();
+    this.defaultMetas = defaultMetas;
+    // built-in data
+    this.setViewPortAuto();
+    this.setHostAuto();
   }
 
   getData() {
@@ -33,13 +58,28 @@ export class AppService {
     return this.data.host || '';
   }
 
+  changeLang(code = 'en') {
+    return document.documentElement.setAttribute('lang', code);
+  }
+
   changeTheme(name?: string) {
     return name
       ? document.body.setAttribute('data-theme', name)
       : document.body.removeAttribute('data-theme');
   }
 
-  private setViewPort() {
+  changePageTitle(title: string) {
+    return this.title.setTitle(title);
+  }
+
+  changePageMetas(customMetas: AppCustomMetas) {
+    const metas = this.processMetaData(customMetas);
+    // set title and meta tags
+    this.changePageTitle(metas.title || 'App');
+    this.changeMetaTags(metas);
+  }
+  
+  private setViewPortAuto() {
     const setViewportHandler = () => {
       this.data.viewWidth = window.innerWidth;
       this.data.viewHeight = window.innerHeight;
@@ -48,13 +88,115 @@ export class AppService {
     setViewportHandler();
   }
 
-  private setHost() {
+  private setHostAuto() {
     const baseHref = ((document.getElementsByTagName('base')[0] || {})['href'] || '').slice(0, -1);
-    if (!!baseHref) {
+    if (baseHref) {
       this.data.host = baseHref;
     } else {
       const hrefSplit = window.location.href.split('/').filter(Boolean);
       this.data.host = hrefSplit[0] + '//' + hrefSplit[1];
+    }
+  }
+
+  private processMetaData(customMetas: AppCustomMetas) {
+    // custom
+    const title = customMetas['title'] || this.defaultMetas['title'];
+    const description = customMetas['description'] || this.defaultMetas['description'];
+    const image = customMetas['image'] || this.defaultMetas['image'];
+    const url = customMetas['url'];
+    const author = customMetas['author'] || this.defaultMetas['author'];
+    const twitterCard = customMetas['twitterCard'] || this.defaultMetas['twitterCard'];
+    const twitterCreator = customMetas['twitterCreator'] || this.defaultMetas['twitterCreator'];
+    const ogType = customMetas['ogType'] || this.defaultMetas['ogType'];
+    const ogLocale = customMetas['ogLocale'] || this.defaultMetas['ogLocale'];
+    // default
+    const twitterSite = this.defaultMetas['twitterSite'];
+    const ogSiteName = this.defaultMetas['ogSiteName'];
+    const fbAppId = this.defaultMetas['fbAppId'];
+    return {
+      title,
+      description,
+      image,
+      url,
+      author,
+      twitterCard,
+      twitterSite,
+      twitterCreator,
+      ogType,
+      ogSiteName,
+      ogLocale,
+      fbAppId,
+    } as AppMetas;
+  }
+
+  private changeHTMLLinkTags(items: Array<{ rel: string, href: string }>) {
+    const dom = window.document;
+    for (const item of items) {
+      const { rel, href } = item;
+      let elem = dom.querySelector(`link[rel=${rel}]`);
+      if (!elem) {
+        // create
+        elem = dom.createElement('link');
+        elem.setAttribute('rel', rel);
+        dom.head.appendChild(elem);
+        elem.setAttribute('href', href);
+      } else {
+        // update
+        elem.setAttribute('href', href);
+      }
+    }
+  }
+
+  private changeMetaTags(metas: AppMetas) {
+    const {
+      title, description, image, url, author,
+      twitterCard, twitterSite, twitterCreator,
+      ogType, ogSiteName, ogLocale, fbAppId,
+    } = metas;
+    // update links and meta
+    if (title) {
+      this.meta.removeTag('itemprop="name"');
+      this.meta.updateTag({ itemprop: 'name', content: title });
+      this.meta.updateTag({ property: 'og:title', content: title });
+    }
+    if (description) {
+      this.meta.removeTag('itemprop="description"');
+      this.meta.updateTag({ name: 'description', content: description });
+      this.meta.updateTag({ itemprop: 'description', content: description });
+      this.meta.updateTag({ property: 'og:description', content: description });
+    }
+    if (image) {
+      this.meta.removeTag('itemprop="image"');
+      this.meta.updateTag({ itemprop: 'image', content: image });
+      this.meta.updateTag({ property: 'og:image', content: image });
+    }
+    if (url) {
+      this.meta.updateTag({ property: 'og:url', content: url });
+      this.changeHTMLLinkTags([{ rel: 'canonical', href: url }]);
+    }
+    if (author) {
+      this.changeHTMLLinkTags([{ rel: 'author', href: author }]);
+    }
+    if (twitterCard) {
+      this.meta.updateTag({ name: 'twitter:card', content: twitterCard });
+    }
+    if (twitterSite) {
+      this.meta.updateTag({ name: 'twitter:site', content: twitterSite });
+    }
+    if (twitterCreator) {
+      this.meta.updateTag({ name: 'twitter:creator', content: twitterCreator });
+    }
+    if (ogType) {
+      this.meta.updateTag({ property: 'og:type', content: ogType });
+    }
+    if (ogSiteName) {
+      this.meta.updateTag({ property: 'og:site_name', content: ogSiteName });
+    }
+    if (ogLocale) {
+      this.meta.updateTag({ property: 'og:locale', content: ogLocale });
+    }
+    if (fbAppId) {
+      this.meta.updateTag({ property: 'fb:app_id', content: fbAppId });
     }
   }
 }
--- projects/useful/src/lib/services/nav/nav.service.ts
+++ projects/useful/src/lib/services/nav/nav.service.ts
@@ -1,84 +1,65 @@
 import { Injectable } from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
 import {
   Router,
   Event,
   RouteConfigLoadStart,
   RouteConfigLoadEnd,
-  NavigationEnd
+  NavigationEnd,
+  NavigationExtras
 } from '@angular/router';
 
-export type RouterEvents = 'RouteConfigLoadStart' | 'RouteConfigLoadEnd' | 'NavigationEnd';
+import { AppService, AppCustomMetas } from '../app/app.service';
 
-export interface CustomMetas {
-  title?: string;
-  description?: string;
-  image?: string;
-  url?: string;
-  author?: string;
-  twitterCard?: string;
-  twitterCreator?: string;
-  ogType?: string;
-  ogLocale?: string;
-}
-
-export interface Metas extends CustomMetas {
-  twitterSite?: string;
-  ogSiteName?: string;
-  fbAppId?: string;
-}
+export type NavRouterEventHooks = 'RouteConfigLoadStart' | 'RouteConfigLoadEnd' | 'NavigationEnd';
 
 export interface NavItem {
   text?: string;
+  html?: string;
   icon?: string;
   classes?: string | string[];
   level?: number;
-  id?: string;
-  href?: string;
+  separator?: true;
+  segment?: string; // inner link
+  href?: string; // normal link
   target?: string;
-  routerLink?: string | string[];
-  handler?: (...args: unknown[]) => void;
+  routerLink?: string | string[]; // router link
+  handler?: (...args: unknown[]) => void; // click
 }
 
+export interface NavMenuItem extends NavItem {
+  subItems?: NavMenuItem[];
+}
+
+export type NavMetaModifier =
+  | string
+  | { (input: Record<string, unknown>): string };
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavService {
   private router?: Router;
-  private data: Record<string, unknown> = {};
-  
+  private routingData: Record<string, unknown> = {};
+  private routingMetaRecords: Record<string, AppCustomMetas> = {};
+
   private loading = false;
   private previousUrls: string[] = [];
 
-  private defaultMetas: Metas = {};
-  private routeMetas: {[url: string]: Metas} = {};
+  constructor(private appService: AppService) {}
 
-  constructor(
-    private title: Title,
-    private meta: Meta,
-  ) {}
-
-  getRouter() {
-    if (!this.router) {
-      throw new Error('No router, please set first!');
-    }
-    return this.router;
-  }
-
-  setRouter(
+  init(
     router: Router,
     hooks: {
-      [key in RouterEvents]?: (event: Event) => void;
+      [key in NavRouterEventHooks]?: (event: Event) => void;
     } = {},
   ) {
     // set router
     this.router = router;
-    // events
+    // register events
     this.router
       .events
       .subscribe(async event => {
-      let eventName = '' as RouterEvents; // event name
+      let eventName = '' as NavRouterEventHooks; // event name
       if (event instanceof RouteConfigLoadStart) {
         eventName = 'RouteConfigLoadStart';
         this.loading = true;
@@ -90,15 +71,14 @@ export class NavService {
         // record urls for backing navigation
         const url = event.urlAfterRedirects;
         const backUrl = this.previousUrls[this.previousUrls.length - 2];
-        if (!backUrl || (!!backUrl && url !== backUrl)) {
+        if (!backUrl || (backUrl && url !== backUrl)) {
           this.previousUrls.push(url);
         } else {
           this.previousUrls.pop();
         }
         // set title & meta
-        this.setMeta(
-          (this.routeMetas[(this.router as Router).url] as Record<string, unknown>)
-          || {}
+        this.appService.changePageMetas(
+          this.routingMetaRecords[(this.router as Router).url] || {}
         );
       }
       // run hook
@@ -108,44 +88,48 @@ export class NavService {
     return this as NavService;
   }
 
-  setDefaultMetas(metas: Metas) {
-    this.defaultMetas = metas;
+  getRouter() {
+    if (!this.router) {
+      throw new Error('No router, please set first!');
+    }
+    return this.router;
+  }
+
+  setMeta(
+    input: Record<string, unknown>,
+    modifiers: Record<string, NavMetaModifier> = {}
+  ) {
+    const customMetas = this.extractCustomMetas(input, modifiers);
+    if (this.router && !this.routingMetaRecords[this.router.url]) {
+      this.routingMetaRecords[this.router.url] = customMetas;
+    }
     return this as NavService;
   }
 
-  /**
-   * data
-   */
   get(key?: string) {
-    return !!key ? this.data[key] : this.data;
+    return key ? this.routingData[key] : this.routingData;
   }
 
-  /**
-   * helpers
-   */
   isLoading() {
     return this.loading;
   }
 
-  canGoBack() {
+  isBacking() {
     return this.router
       && this.previousUrls.length > 1
       && this.router.url !== ''
       && this.router.url !== '/';
   }
 
-  /**
-   * navigation
-   */
   back() {
     const [ path, ... queryStringArr ] = (this.previousUrls[this.previousUrls.length - 2] || '/').split('?');
     // query params
     const queryParams = {} as Record<string, unknown>;
-    if (!!queryStringArr.length) {
+    if (queryStringArr.length) {
       const queryItems = queryStringArr.join('?').split('&');
       for (const queryItem of queryItems) {
         const [ key, value ] = queryItem.split('=');
-        if (!!key && !!value) {
+        if (key && value) {
           queryParams[key] = value;
         }
       }
@@ -156,10 +140,10 @@ export class NavService {
 
   navigate(
     route: string[],
-    data: {[key: string]: any} = {},
-    navigationExtras: any = {},
+    data: Record<string, unknown> = {},
+    navigationExtras: NavigationExtras,
   ) {
-    this.data = data;
+    this.routingData = data; // set route data
     return this.getRouter().navigate(route, navigationExtras);
   }
 
@@ -175,134 +159,36 @@ export class NavService {
     return elm?.scrollIntoView({ behavior: 'smooth' });
   }
 
-  /**
-   * meta
-   */
-
-  setMeta(
-    data: Record<string, unknown> = {},
-    rewriteFields: Record<string, unknown> = {}
+  private extractCustomMetas(
+    input: Record<string, unknown>,
+    modifiers: Record<string, NavMetaModifier> = {}
   ) {
-    // extract & process meta data
-    const customMetas = this.extractMetaData(data, rewriteFields);
-    const metas = this.processMetaData(customMetas);
-    // save meta data
-    if (this.router && !this.routeMetas[this.router.url]) {
-      this.routeMetas[this.router.url] = customMetas;
-    }
-    // set title and meta tags
-    this.setTitle(metas.title || 'App');
-    this.setTags(metas);
-  }
-
-  private setTitle(newTitle: string) {
-    return this.title.setTitle(newTitle);
-  }
-
-  private setTags(data: Metas) {
-    const {
-      title, description, image, url, author,
-      twitterCard, twitterSite, twitterCreator,
-      ogType, ogSiteName, ogLocale, fbAppId,
-    } = data;
-    // update links and meta
-    if (!!title) {
-      this.meta.removeTag('itemprop="name"');
-      this.meta.updateTag({ itemprop: 'name', content: title });
-      this.meta.updateTag({ property: 'og:title', content: title });
-    }
-    if (!!description) {
-      this.meta.removeTag('itemprop="description"');
-      this.meta.updateTag({ name: 'description', content: description });
-      this.meta.updateTag({ itemprop: 'description', content: description });
-      this.meta.updateTag({ property: 'og:description', content: description });
-    }
-    if (!!image) {
-      this.meta.removeTag('itemprop="image"');
-      this.meta.updateTag({ itemprop: 'image', content: image });
-      this.meta.updateTag({ property: 'og:image', content: image });
-    }
-    if (!!url) {
-      this.meta.updateTag({ property: 'og:url', content: url });
-      this.setLinks([{ rel: 'canonical', href: url }]);
-    }
-    if (!!author) {
-      this.setLinks([{ rel: 'author', href: author }]);
-    }
-    if (!!twitterCard) {
-      this.meta.updateTag({ name: 'twitter:card', content: twitterCard });
-    }
-    if (!!twitterSite) {
-      this.meta.updateTag({ name: 'twitter:site', content: twitterSite });
-    }
-    if (!!twitterCreator) {
-      this.meta.updateTag({ name: 'twitter:creator', content: twitterCreator });
-    }
-    if (!!ogType) {
-      this.meta.updateTag({ property: 'og:type', content: ogType });
-    }
-    if (!!ogSiteName) {
-      this.meta.updateTag({ property: 'og:site_name', content: ogSiteName });
-    }
-    if (!!ogLocale) {
-      this.meta.updateTag({ property: 'og:locale', content: ogLocale });
-    }
-    if (!!fbAppId) {
-      this.meta.updateTag({ property: 'fb:app_id', content: fbAppId });
-    }
-  }
-
-  private setLinks(items: Array<{ rel: string, href: string }>) {
-    const dom = window.document;
-    for (const item of items) {
-      const { rel, href } = item;
-      let elem = dom.querySelector(`link[rel=${rel}]`);
-      if (!elem) {
-        // create
-        elem = dom.createElement('link');
-        elem.setAttribute('rel', rel);
-        dom.head.appendChild(elem);
-        elem.setAttribute('href', href);
-      } else {
-        // update
-        elem.setAttribute('href', href);
-      }
-    }
-  }
-
-  private extractMetaData(data: any, rewriteFields: any = {}): CustomMetas {
-    const title = data[rewriteFields['title'] || 'title'];
-    const description = data[rewriteFields['description'] || 'description'];
-    const image = data[rewriteFields['image'] || 'image'];
-    let url: string = data[rewriteFields['url'] || 'url'] || window.document.URL;
-    url = url.substr(-1) === '/' ? url : (url + '/');
-    const author = data[rewriteFields['author'] || 'author'];
-    const twitterCard = data[rewriteFields['twitterCard'] || 'twitterCard'];
-    const twitterCreator = data[rewriteFields['twitterCreator'] || 'twitterCreator'];
-    const ogType = data[rewriteFields['ogType'] || 'ogType'];
-    const ogLocale = data[rewriteFields['ogLocale'] || 'ogLocale'];
-    return { title, description, image, url, author, twitterCard, twitterCreator, ogType, ogLocale };
-  }
-
-  private processMetaData(customMetas: CustomMetas): Metas {
-    // custom
-    const title = customMetas['title'] || this.defaultMetas['title'];
-    const description = customMetas['description'] || this.defaultMetas['description'];
-    const image = customMetas['image'] || this.defaultMetas['image'];
-    const url = customMetas['url'];
-    const author = customMetas['author'] || this.defaultMetas['author'];
-    const twitterCard = customMetas['twitterCard'] || this.defaultMetas['twitterCard'];
-    const twitterCreator = customMetas['twitterCreator'] || this.defaultMetas['twitterCreator'];
-    const ogType = customMetas['ogType'] || this.defaultMetas['ogType'];
-    const ogLocale = customMetas['ogLocale'] || this.defaultMetas['ogLocale'];
-    // default
-    const twitterSite = this.defaultMetas['twitterSite'];
-    const ogSiteName = this.defaultMetas['ogSiteName'];
-    const fbAppId = this.defaultMetas['fbAppId'];
-    return {
-      title, description, image, url, author,
-      twitterCard, twitterSite, twitterCreator,
-      ogType, ogSiteName, ogLocale, fbAppId,
+    const getMetaValue = (fieldName: string) => {
+      const modifier = modifiers[fieldName];
+      return modifier && modifier instanceof Function
+        ? modifier(input)
+        : input[modifier || fieldName] as string;
     };
+    const title = getMetaValue('title');
+    const description = getMetaValue('description');
+    const image = getMetaValue('image');
+    let url = getMetaValue('url') || window.document.URL;
+    url = url.substr(-1) === '/' ? url : (url + '/');
+    const author = getMetaValue('author');
+    const twitterCard = getMetaValue('twitterCard');
+    const twitterCreator = getMetaValue('twitterCreator');
+    const ogType = getMetaValue('ogType');
+    const ogLocale = getMetaValue('ogLocale');
+    return {
+      title,
+      description,
+      image,
+      url,
+      author,
+      twitterCard,
+      twitterCreator,
+      ogType,
+      ogLocale
+    } as AppCustomMetas;
   }
 }
