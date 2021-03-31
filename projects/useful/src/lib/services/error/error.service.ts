bf534ad088808510be14cd74c75be4fdf5d4541c
Lam Nhan <lamnhan92@gmail.com>
2021-03-31 10:34:23 +0700

    error default service to web

--- projects/useful/src/lib/services/error/error.service.ts
+++ projects/useful/src/lib/services/error/error.service.ts
@@ -58,7 +58,7 @@ export class ErrorService {
     }
     const payload = {
       serviceContext: {
-        service: service || 'app',
+        service: service || 'Web',
         ...(version ? { version } : undefined)
       },
       context: {
