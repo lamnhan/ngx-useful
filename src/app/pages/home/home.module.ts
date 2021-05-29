302855131085c66ad2fe8e5f34493117067192a7
Lam Nhan <lamnhan92@gmail.com>
2021-05-29 12:42:12 +0700

    'deploy:app'

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkDirectiveModule } from '@lamnhan/ngx-useful';
 
+import { ContentComponentModule } from '../../components/content/content.module';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
@@ -12,6 +14,7 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     RouterLinkDirectiveModule,
+    ContentComponentModule,
     HomeRoutingModule
   ]
 })
